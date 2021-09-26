4a18e07ec072ae6f8f56c7d3e12f1c36278bd820
Orrin Manning <orrin.manning@protonmail.com>
2021-09-26 13:55:12 -0500

    Clean up the flow of the code

--- chess2.py
+++ chess2.py
@@ -4,32 +4,24 @@ import sys, pygame
 from pygame.locals import *
 
 SCREENRECT = pygame.Rect(0, 0, 800, 600)
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
 
 def main():
-    white = (255, 255, 255)
-    red = (255, 0, 0, 35)
-    black = (0, 0, 0)
     # Initialize pygame module
     pygame.init()
 
-    # Loading images
-    w_king_sprite = pygame.image.load('assets/white_king.png')
-    w_queen_sprite = pygame.image.load('assets/white_queen.png')
-    w_rook_sprite = pygame.image.load('assets/white_rook.png')
-    w_bishop_sprite = pygame.image.load('assets/white_bishop.png')
-    w_knight_sprite = pygame.image.load('assets/white_knight.png')
-    w_pawn_sprite = pygame.image.load('assets/white_pawn.png')
-    b_king_sprite = pygame.image.load('assets/black_king.png')
-    b_queen_sprite = pygame.image.load('assets/black_queen.png')
-    b_rook_sprite = pygame.image.load('assets/black_rook.png')
-    b_bishop_sprite = pygame.image.load('assets/black_bishop.png')
-    b_knight_sprite = pygame.image.load('assets/black_knight.png')
-    b_pawn_sprite = pygame.image.load('assets/black_pawn.png')
-
+	# Load/scale board image and get its size
     board_sprite = pygame.image.load('assets/board.png')
     for i in range(2):
         board_sprite = pygame.transform.scale2x(board_sprite)
     board_size = np.array(board_sprite.get_size())
+
+    # Create the screen
+    winstyle = 0
+    bestdepth = pygame.display.mode_ok(board_size, winstyle, 32)
+    screen = pygame.display.set_mode(board_size, winstyle, bestdepth)
 
     # Create board bounds as sub-surface of board
     board_bounds_size = np.array(board_sprite.get_size()) * .995
@@ -47,10 +39,19 @@ def main():
     #     for rank in range(8):
     #         square = 
 
-    winstyle = 0
-    bestdepth = pygame.display.mode_ok(board_size, winstyle, 32)
-    screen = pygame.display.set_mode(board_size, winstyle, bestdepth)
-    screen.fill(black)
+    # Loading images
+    w_king_sprite = pygame.image.load('assets/white_king.png')
+    w_queen_sprite = pygame.image.load('assets/white_queen.png')
+    w_rook_sprite = pygame.image.load('assets/white_rook.png')
+    w_bishop_sprite = pygame.image.load('assets/white_bishop.png')
+    w_knight_sprite = pygame.image.load('assets/white_knight.png')
+    w_pawn_sprite = pygame.image.load('assets/white_pawn.png')
+    b_king_sprite = pygame.image.load('assets/black_king.png')
+    b_queen_sprite = pygame.image.load('assets/black_queen.png')
+    b_rook_sprite = pygame.image.load('assets/black_rook.png')
+    b_bishop_sprite = pygame.image.load('assets/black_bishop.png')
+    b_knight_sprite = pygame.image.load('assets/black_knight.png')
+    b_pawn_sprite = pygame.image.load('assets/black_pawn.png')
 
     running = True
     while running:
